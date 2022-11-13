cd104015eec3793851beb5d6764a7d6365801c52
Harsh-Singh-Rajput <hs98215479@gmail.com>
2022-11-13 17:24:38 +0530

    Added index.html main.js style.css

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector("#currency-converter")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const {
+        target: { from, to, amount },
+      } = event;
+
+      var headers = new Headers();
+      headers.append("apikey", "4FD0SJ8VLzKrG4Ik9jHsr1yl4ZTfWYLq");
+
+      const requestOptions = {
+        method: "GET",
+        headers,
+      };
+
+
+      const data = fetch(
+        `https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let {
+            info,
+            date,
+            query: { to },
+            result,
+          } = data;
+          document.getElementById("result").style.display = "block";
+          document.getElementById(
+            "exchange-rate"
+          ).textContent = `As per the exchange rate: ${info.rate}`;
+          document.getElementById("date").textContent = `For Date: ${date}`;
+          document.getElementById(
+            "conversion"
+          ).textContent = `Convertd Value in ${to} is: ${result.toFixed(2)}`;
+        });
+    });
+});
